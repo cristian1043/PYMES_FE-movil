c3927251794867c972d19813233878f36ef61f49
DUBONACO <garavitojimenezcristian@gmail.com>
2026-08-29 17:21:38 -0500

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
@@ -16,5 +16,6 @@ import { AppRoutingModule } from './app-routing.module';
     provideHttpClient()
   ],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
