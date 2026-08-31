fe4f46a4cb5a75a3b52eb5c2f29d55d82de78a0f
DUBONACO <garavitojimenezcristian@gmail.com>
2026-08-30 20:59:47 -0500

    yes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
+const host = (typeof window !== 'undefined' && window.location.hostname && window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1')
+  ? window.location.hostname
+  : '192.168.1.26';
+
 export const environment = {
   production: false,
-  apiUrl: '/api'
+  apiUrl: `http://${host}:5000/api`
 };
