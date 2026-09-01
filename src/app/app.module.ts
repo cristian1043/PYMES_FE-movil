edc8b194c43a3606d25a71a6e0730648f707772c
DUBONACO <garavitojimenezcristian@gmail.com>
2026-09-01 16:36:32 -0500

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular/lazy';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
     provideHttpClient()
   ],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,15 @@
-const host = (typeof window !== 'undefined' && window.location.hostname && window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1')
-  ? window.location.hostname
-  : '192.168.1.26';
+const getApiUrl = (): string => {
+  if (typeof window === 'undefined') {
+    return 'http://127.0.0.1:5000/api';
+  }
+  const hostname = window.location.hostname;
+  if (hostname === 'localhost' || hostname === '127.0.0.1') {
+    return 'http://localhost:5000/api';
+  }
+  return `http://${hostname}:5000/api`;
+};
 
 export const environment = {
   production: false,
-  apiUrl: `http://${host}:5000/api`
+  apiUrl: getApiUrl()
 };
